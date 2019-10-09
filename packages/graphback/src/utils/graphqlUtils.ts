5173486e1dbe27dc2a5568e1d47778866451e6df
Enda Phelan <ephelan@redhat.com>
2019-10-09 15:18:51 +0100

    feat: add schema file processing util functions

--- packages/graphback/src/utils/graphqlUtils.ts
+++ packages/graphback/src/utils/graphqlUtils.ts
@@ -1,6 +1,16 @@
-import { ResolverType } from '../generators/resolvers';
-import { InputModelTypeContext, INTERFACE_TYPE_DEFINITION, OBJECT_TYPE_DEFINITION, OBJECT_TYPE_EXTENSION } from '../input/ContextTypes';
+import { INTERFACE_TYPE_DEFINITION, OBJECT_TYPE_DEFINITION, OBJECT_TYPE_EXTENSION, InputModelTypeContext } from '../input/ContextTypes';
+import { sync } from 'glob';
+import { join } from 'path';
+import { readFileSync } from 'fs';
+import { GraphQLSchema, buildSchema } from 'graphql';
 
+export enum ResolverType {
+  CREATE = 'create',
+  UPDATE = 'update',
+  FIND = 'find',
+  FIND_ALL = 'findAll',
+  DELETE = 'delete',
+}
 
 export const getFieldName = (typeName: string, action: ResolverType, plural: string = ''): string => {
   const upperCasedType = typeName.charAt(0).toUpperCase() + typeName.substr(1);
@@ -27,4 +37,39 @@ export const filterObjectExtensions = (types: InputModelTypeContext[]) => types.
  */
 export const createImplementsInterfaceString = (names: string[]) => {
   return `implements ${names.map((name: string) => name).join(' & ')} `;
+}
+
+/**
+ * Collects all GraphQL files in a directory and reads the content into a string.
+ *
+ * @param schemaDir - The directory of the schema file(s)
+ */
+export const buildSchemaText = (schemaDir: string): string => {
+  const schemaPath = join(schemaDir, '*.graphql');
+  const files = sync(schemaPath);
+
+  if (files.length === 0) {
+    return undefined;
+  }
+
+  const schemaText = files
+    .map((f: string) => readFileSync(f))
+    .join('\n');
+
+  return schemaText.length ? schemaText : undefined;
+}
+
+/**
+ * Builds a GraphQLSchema object from all .graphql files in a directory
+ *
+ * @param schemaDir - The directory of the schema files
+ */
+export const buildSchemaFromDir = (schemaDir: string): GraphQLSchema => {
+  const schemaText = buildSchemaText(schemaDir);
+
+  if (!schemaText) {
+    return undefined;
+  }
+
+  return buildSchema(schemaText);
 }
